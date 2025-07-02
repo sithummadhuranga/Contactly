60633deb7cbdf19105ac4cf359da715ddffc26f2
Sithum Madhuranga <sithummadhu.uni@gmail.com>
2025-07-02 14:35:19 +0530

    contact

---
+++ API/Contactly/Contactly/Controllers/ContactsController.cs
@@ -0,0 +1,45 @@
+﻿using Contactly.Data;
+using Contactly.Models;
+using Contactly.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contactly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactlyDbContext dbContext;
+
+        public ContactsController(ContactlyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequestDTO request)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favourite = request.Favourite
+            };
+            dbContext.Contacts.Add(domainModelContact);
+            dbContext.SaveChanges();
+
+            return Ok(domainModelContact);
+        }
+
+    }
+}
--- API/Contactly/Contactly/Program.cs
+++ API/Contactly/Contactly/Program.cs
@@ -1,3 +1,6 @@
+using Contactly.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ContactlyDbContext>(options =>
+options.UseInMemoryDatabase("ContactsDv"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
